09a5fe18e6518c4301d0948cbd1fc34d96cbb3cb
Calum J. Eadie <calum@calumjeadie.com>
2013-12-15 21:33:36 +0000

    Added basic edit functionality

--- done/done.py
+++ done/done.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from os.path import join
 import string
 import datetime
 
@@ -8,6 +9,7 @@ from PySide.QtGui import *
 
 # Configuration
 TODO_DIR = os.path.expanduser("~/todo")
+TODO_FILE = join(TODO_DIR, "todo.txt")
 
 # STYLESHEET = """
 # QToolBar QWidget
@@ -21,13 +23,15 @@ class Done(QMainWindow):
     def __init__(self):
         super(Done, self).__init__()
 
+        self._todoListFile = open(TODO_FILE, "r+")
+
         self._initUI()
 
     def _initUI(self):
 
         # Set up window.
 
-        self.setWindowTitle("Done")
+        self.setWindowTitle(TODO_FILE)
         self.resize(400, 400)
         # self.setStyleSheet(STYLESHEET)
 
@@ -37,8 +41,12 @@ class Done(QMainWindow):
 
         vboxLayout = QVBoxLayout()
 
-        self._notesView = QPlainTextEdit()
-        vboxLayout.addWidget(self._notesView)
+        self._todoListEdit = QPlainTextEdit()
+        todoList = self._todoListFile.read().decode('utf8')
+        self._todoListEdit.setPlainText(todoList)
+        self._todoListEdit.textChanged.connect(self._saveTodoList)
+
+        vboxLayout.addWidget(self._todoListEdit)
 
         centralWidget.setLayout(vboxLayout)
 
@@ -46,6 +54,12 @@ class Done(QMainWindow):
 
         # Show window.
         self.show()
+
+    def _saveTodoList(self):
+        todoList = self._todoListEdit.toPlainText().encode('utf8')
+        self._todoListFile.seek(0)
+        self._todoListFile.write(todoList)
+        self._todoListFile.truncate()
 
 
 def main():
