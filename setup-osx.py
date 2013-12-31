27dd16d93f9d2c703faa9163f789ec5693cc666b
Calum J. Eadie <calum@calumjeadie.com>
2013-12-31 17:23:48 +0000

    Added build script for mac osx

---
+++ setup-osx.py
@@ -0,0 +1,14 @@
+"""
+Based on:
+- https://github.com/tam7t/markdown-editor/blob/master/setup-osx.py
+- https://github.com/tam7t/photograbber/blob/master/setup-osx.py
+
+Usage: python setup-osx.py py2app
+"""
+
+from setuptools import setup
+
+setup(
+    setup_requires=['py2app'],
+    app=['done/done.py']
+)
